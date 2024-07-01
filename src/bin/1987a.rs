15f137230c3478c34d3b3fbfb90145befeeef160
barrydevp <barrydevp@gmail.com>
2024-07-01 15:51:01 -0400

    codeforces: EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2). AC: A, B, C, D(late)

---
+++ src/bin/1987a.rs
@@ -0,0 +1,53 @@
+// https://codeforces.com/contest/1987/problem/A
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    #[allow(dead_code)]
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+    #[allow(dead_code)]
+    fn next_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.next::<T>()).collect()
+    }
+    #[allow(dead_code)]
+    fn next_chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
+    }
+    #[allow(dead_code)]
+    fn next_line(&mut self) -> String {
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    let nt = scan.next::<usize>();
+    for _ in 0..nt {
+        let n = scan.next::<usize>();
+        let k = scan.next::<usize>();
+
+        let mut ans = (n - 1) * k + 1;
+
+        writeln!(out, "{}", ans).ok();
+    }
+}
