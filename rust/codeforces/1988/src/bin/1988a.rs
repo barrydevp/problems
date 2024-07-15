1d303ddaf8cd7e7cc14eca68ca7ecd1c74f7b07b
barrydevp <barrydevp@gmail.com>
2024-07-15 14:36:36 -0400

    codeforces: Codeforces Round 958 (Div. 2). AC: A, B, C(late)

---
+++ rust/codeforces/1988/src/bin/1988a.rs
@@ -0,0 +1,62 @@
+// https://codeforces.com/contest/1988/problem/A
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    #[allow(dead_code)]
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+    #[allow(dead_code)]
+    fn next_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.next::<T>()).collect()
+    }
+    #[allow(dead_code)]
+    fn next_chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
+    }
+    #[allow(dead_code)]
+    fn next_line(&mut self) -> String {
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    let nt = scan.next::<usize>();
+    for _ in 0..nt {
+        let mut n = scan.next::<i32>();
+        let k = scan.next::<i32>();
+
+        if n == 1 {
+            writeln!(out, "{}", 0).ok();
+        } else {
+            let mut ans = 0;
+            while n > 1 {
+                ans += 1;
+                n -= (k - 1);
+            }
+
+            writeln!(out, "{}", ans).ok();
+        }
+    }
+}
+
---
+++ rust/codeforces/1988/src/bin/1988c.rs
@@ -0,0 +1,65 @@
+// https://codeforces.com/contest/1988/problem/C
+
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    #[allow(dead_code)]
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+    #[allow(dead_code)]
+    fn next_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.next::<T>()).collect()
+    }
+    #[allow(dead_code)]
+    fn next_chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
+    }
+    #[allow(dead_code)]
+    fn next_line(&mut self) -> String {
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    let nt = scan.next::<usize>();
+    for _ in 0..nt {
+        let n = scan.next::<u64>();
+        let mut a = vec![];
+
+        for i in (0..=63).rev() {
+            let x = 1 << i;
+            if x <= n {
+                if (n & x) == x && (n - x) != n && (n - x) != 0 {
+                    a.push(n - x);
+                }
+            }
+        }
+        a.push(n);
+
+        writeln!(out, "{}", a.len()).ok();
+        a.iter().for_each(|x| {
+            write!(out, "{} ", x).ok();
+        });
+        writeln!(out).ok();
+    }
+}
